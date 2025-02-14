987a85443fdc72a82c9489ac2faa4aaf3e608157
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-02-14 22:42:37 +0200

    make_str: progress

--- src/make_str.js
+++ src/make_str.js
@@ -1,9 +1,12 @@
 function make_str(value, default_value = '')
 {
+    // if (value === null || value === undefined || value === Number.POSITIVE_INFINITY || value === Number.NEGATIVE_INFINITY || Number.isNaN(value)) {
+    //     return default_value;
+    // }
     if (typeof value !== 'string') {
         return default_value;
     }
-    return String(value);
+    return value;
 }
 
 module.exports = make_str;
