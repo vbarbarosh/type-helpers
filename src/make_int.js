75eb1277f6dee180da42f645ca770615195fdbb4
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-02-15 22:36:42 +0200

    make_int: stick to Math.trunc

--- src/make_int.js
+++ src/make_int.js
@@ -1,3 +1,6 @@
+// Returning default_value as is will allow the following:
+//     make_int(v.width, null, 0, 4096)
+//
 function make_int(value, default_value = 0, min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER)
 {
     switch (typeof value) {
@@ -9,7 +12,7 @@ function make_int(value, default_value = 0, min = Number.MIN_SAFE_INTEGER, max =
     if (value === null || value === undefined || !isFinite(value)) {
         return default_value;
     }
-    return Math.max(min, Math.min(max, Math.round(value === 0 ? value : (+value||default_value))));
+    return Math.max(min, Math.min(max, Math.trunc(value === 0 ? value : (+value||default_value))));
 }
 
 module.exports = make_int;
