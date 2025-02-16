0af133ae82c43be27fd0970f182615455ba79eda
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-02-16 14:23:37 +0200

    make_obj: handle edge values

--- src/make_obj.js
+++ src/make_obj.js
@@ -1,6 +1,18 @@
+/**
+ * The main use case was to convert a value into something, which could be used with dot expression:
+ *
+ *     make_obj(val).xxx
+ *
+ * 🔦 Modern JavaScript have a better alternative:
+ *
+ *     val?.xxx
+ */
 function make_obj(value, default_value = {})
 {
-    return (value !== null && typeof value === 'object') ? value : default_value;
+    if (value === null || typeof value !== 'object') {
+        return default_value;
+    }
+    return value;
 }
 
 module.exports = make_obj;
---
+++ src/make_obj.test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const edge_values = require('./edge_values');
+const make_obj = require('./make_obj');
+
+const SP = Symbol('default_value for make_obj');
+
+describe('make_obj', function () {
+    it('should accept no args', function () {
+        assert.deepStrictEqual(make_obj(), {});
+    });
+    it('should return the default value for null, undefined, or NaN', function () {
+        assert.strictEqual(make_obj(null, SP), SP);
+        assert.strictEqual(make_obj(undefined, SP), SP);
+        assert.strictEqual(make_obj(NaN, SP), SP);
+    });
+    describe('should handle edge values', function () {
+        edge_values.forEach(function (item) {
+            it(item.label, function () {
+                switch (item.label) {
+                case '/./':
+                case '{}':
+                case '[]':
+                    assert.strictEqual(make_obj(item.value, SP), item.value);
+                    break;
+                default:
+                    assert.strictEqual(make_obj(item.value, SP), SP);
+                    break;
+                }
+            });
+        });
+    });
+});
