c70c6bd71dc674c848f82043a7fa6c641fd12f8d
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-02-16 16:28:59 +0200

    make_float: test suite

--- src/make_float.js
+++ src/make_float.js
@@ -1,9 +1,26 @@
+const is_array = require('./is_array');
+
 function make_float(value, default_value = 0, min = -Number.MAX_VALUE, max = Number.MAX_VALUE)
 {
-    if (value === null || value === undefined || isNaN(value)) {
+    if (value === null || value === undefined || is_array(value) || Number.isNaN(value)) {
         return default_value;
     }
-    return Math.max(min, Math.min(max, +value||0));
+
+    switch (typeof value) {
+    case 'bigint':
+        return Math.max(min, Math.min(max, Number(value)));
+    case 'symbol':
+    case 'function':
+        return default_value;
+    }
+
+    const tmp = value*1;
+    if (Number.isNaN(tmp)) {
+        return default_value;
+    }
+
+    // ||0 to convert -0 to 0
+    return Math.max(min, Math.min(max, tmp||0));
 }
 
 module.exports = make_float;
---
+++ src/make_float.test.js
@@ -0,0 +1,70 @@
+const assert = require('assert');
+const edge_values = require('./edge_values');
+const make_float = require('./make_float');
+
+const SP = Symbol('default_value for make_float');
+
+describe('make_float', function () {
+    it('should accept no args', function () {
+        assert.strictEqual(make_float(), false);
+    });
+    it('should return the default value for null, undefined, or NaN', function () {
+        assert.strictEqual(make_float(null, SP), SP);
+        assert.strictEqual(make_float(undefined, SP), SP);
+        assert.strictEqual(make_float(NaN, SP), SP);
+    });
+    describe('should handle edge values', function () {
+        edge_values.forEach(function (item) {
+            it(item.label, function () {
+                switch (item.label) {
+                case 'null':
+                case 'undefined':
+                case 'NaN':
+                    assert.strictEqual(make_float(item.value, SP), SP);
+                    break;
+                case "''":
+                case 'false':
+                case '0':
+                case '-0':
+                case '0n':
+                    assert.strictEqual(make_float(item.value, SP), 0);
+                    break;
+                case '0.49':
+                case '0.50':
+                case '0.51':
+                case '-0.49':
+                case '-0.50':
+                case '-0.51':
+                case '1e100':
+                case '1e-100':
+                case 'Number.MIN_VALUE':
+                case 'Number.MAX_VALUE':
+                case 'Number.MIN_SAFE_INTEGER':
+                case 'Number.MAX_SAFE_INTEGER':
+                    assert.strictEqual(make_float(item.value, SP), item.value);
+                    break;
+                case '10n**100n':
+                    assert.strictEqual(make_float(item.value, SP), 1e100);
+                    break;
+                case '-(10n**100n)':
+                    assert.strictEqual(make_float(item.value, SP), -1e100);
+                    break;
+                case 'true':
+                    assert.strictEqual(make_float(item.value, SP), 1);
+                    break;
+                case 'Infinity':
+                case 'Number.POSITIVE_INFINITY':
+                    assert.strictEqual(make_float(item.value, SP), Number.MAX_VALUE);
+                    break;
+                case '-Infinity':
+                case 'Number.NEGATIVE_INFINITY':
+                    assert.strictEqual(make_float(item.value, SP), -Number.MAX_VALUE);
+                    break;
+                default:
+                    assert.strictEqual(make_float(item.value, SP), SP);
+                    break;
+                }
+            });
+        });
+    });
+});
