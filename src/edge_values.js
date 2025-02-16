eeb48cf292f1f5a1411bc29eb5ec8aa8a1a29b46
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-02-16 15:24:31 +0200

    edge_values: notes on NaN, Infinity, and -Infinity

--- src/edge_values.js
+++ src/edge_values.js
@@ -40,9 +40,24 @@ const edge_values = [
     {label: '-0', value: -0},
     {label: '-(10n**100n)', value: -(10n**100n)},
     {label: '1e-100', value: 1e-100},
-    {label: 'NaN', value: NaN},
-    {label: 'Infinity', value: Infinity},
-    {label: '-Infinity', value: -Infinity},
+    {label: 'NaN', value: NaN, description: `
+        Result of invalid number operations:
+          - const nan1 = 0 / 0;
+          - const nan2 = Number('not a number');
+          - const nan3 = Math.sqrt(-1);
+    `},
+    {label: 'Infinity', value: Infinity, description: `
+        Division by zero:
+          - const infinity1 = 1 / 0;
+        Exceeding the maximum number:
+          - const infinity2 = Number.MAX_VALUE * 2;
+    `},
+    {label: '-Infinity', value: -Infinity, description: `
+        Negative division by zero:
+          - const negInfinity1 = -1 / 0;
+        Exceeding negative range:
+          - const negInfinity2 = -Number.MAX_VALUE * 2;
+    `},
     {label: 'Number.MIN_VALUE', value: Number.MIN_VALUE},
     {label: 'Number.MAX_VALUE', value: Number.MAX_VALUE},
     {label: 'Number.MIN_SAFE_INTEGER', value: Number.MIN_SAFE_INTEGER},
